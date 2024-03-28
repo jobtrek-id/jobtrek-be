0bfcd9fa6da805c78991d8a68e5feaf00b979d6e
farkhanmaul <farkhanmaul@gmail.com>
2024-03-29 06:13:31 +0700

    feat(starting):router

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-   res.send("Hello World!");
-});
+require("dotenv").config();
+const port = process.env.APP_PORT || 3030;
+
+const router = require("./route");
+app.use("/", router);
 
 app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`);
+   console.log(`Running ons port ${port}`);
 });
