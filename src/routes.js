f47e84ace5fad0b201e1ce1998e7c2a63f30b636
farkhanmaul <farkhanmaul@gmail.com>
2024-04-20 22:08:15 +0700

    define routes

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,13 @@ const controller = require("./controller");
 // define the home page route
 router.get("/", controller.endpoint);
 
+// define the user routes
+router.post("/user", controller.user);
+
 // define the job routes
 router.post("/job/select", controller.jobSelect);
 router.post("/job/delete", controller.jobDelete);
 router.post("/job/update", controller.jobUpdate);
 router.post("/job/insert", controller.jobInsert);
-
-router.post("/user", controller.user);
 
 module.exports = router;
